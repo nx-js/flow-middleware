c1ab9b3aba42690f6613fc7886e063f183847d59
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-01-02 15:48:20 +0100

    initial commit

---
+++ flow.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const dom = require('@nx-js/dom-util')
+
+const secret = {
+  showing: Symbol('flow showing'),
+  prevArray: Symbol('flow prevArray'),
+  trackBy: Symbol('track by')
+}
+
+function flow (elem) {
+  if (elem.nodeType !== 1) return
+
+  const hasIf = elem.$hasAttribute('if')
+  const hasRepeat = elem.$hasAttribute('repeat')
+
+  if (hasIf && hasRepeat) {
+    throw new Error('if and repeat attributes can not be used on the same element')
+  }
+  if (hasIf || hasRepeat) {
+    dom.normalizeContent(elem)
+    dom.extractContent(elem)
+  }
+
+  elem.$attribute('if', ifAttribute)
+  elem.$attribute('track-by', trackByAttribute)
+  elem.$attribute('repeat', repeatAttribute)
+}
+flow.$name = 'flow'
+flow.$require = ['attributes']
+module.exports = flow
+
+function ifAttribute (show) {
+  if (show && !this[secret.showing]) {
+    dom.insertContent(elem)
+    this[secret.showing] = true
+  } else if (!show && this[secret.showing]) {
+    dom.clearContent(elem)
+    this[secret.showing] = false
+  }
+}
+
+function trackByAttribute (trackBy) {
+  this[secret.trackBy] = trackBy
+}
+
+function repeatAttribute (array) {
+  const repeatValue = this.getAttribute('repeat-value') || '$value'
+  const repeatIndex = this.getAttribute('repeat-index') || '$index'
+
+  let trackBy = this[secret.trackBy] || isSame
+  let trackByProp
+  if (typeof trackBy === 'string') {
+    trackByProp = trackBy
+    trackBy = isSame
+  }
+
+  array = array || []
+  const prevArray = this[secret.prevArray] = this[secret.prevArray] || []
+
+  let i = -1
+  iteration: for (let item of array) {
+    let prevItem = prevArray[++i]
+
+    if (prevItem === item) {
+      continue
+    }
+    if (trackBy(item, prevItem, trackByProp)) {
+      dom.mutateContext(this, i, {[repeatValue]: item})
+      prevArray[i] = item
+      continue
+    }
+    for (let j = i + 1; j < prevArray.length; j++) {
+      prevItem = prevArray[j]
+      if (trackBy(item, prevItem, trackByProp)) {
+        dom.moveContent(this, j, i, {[repeatIndex]: i})
+        prevArray.splice(i, 0, prevItem)
+        prevArray.splice(j, 1)
+        continue iteration
+      }
+    }
+    dom.insertContent(this, i, {[repeatIndex]: i, [repeatValue]: item})
+    prevArray.splice(i, 0, item)
+  }
+
+  if ((++i) === 0) {
+    prevArray.length = 0
+    dom.clearContent(this)
+  } else {
+    while (i < prevArray.length) {
+      dom.removeContent(this)
+      prevArray.pop()
+    }
+  }
+}
+
+function isSame (item1, item2, prop) {
+  return (item1 === item2 ||
+    (prop && typeof item1 === 'object' && typeof item2 === 'object' &&
+    item1 && item2 && item1[prop] === item2[prop]))
+}
